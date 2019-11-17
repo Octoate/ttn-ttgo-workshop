f7f912414dd956dc827b0689ee3f4b6cbea4c600
Tim Riemann <triemann@octoate.de>
2019-11-17 13:14:45 +0100

    add function definitions

--- projecttemplate/src/main.cpp
+++ projecttemplate/src/main.cpp
@@ -1,9 +1,43 @@
 #include <Arduino.h>
 
-void setup() {
+// function declarations
+void buildDataContainer();
+void readSensorData();
+void displaySensorData();
+
+void setup() 
+{
   // put your setup code here, to run once:
 }
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
+  readSensorData();
+  displaySensorData();
+  buildDataContainer();
+}
+
+/*
+ * Prepare sensordata for LoRaWAN transmission
+ */
+void buildDataContainer() 
+{
+
+}
+
+/*
+ * Read sensor data
+ */
+void readSensorData() 
+{
+  
+}
+
+/*
+ * Show sensor data on the display
+ */
+void displaySensorData() 
+{
+  
 }
