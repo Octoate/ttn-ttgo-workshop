5f9237cda978cec134c97e45d64feeed03e463fe
Tim Riemann <triemann@octoate.de>
2019-11-18 19:44:56 +0100

    serial debug output

--- projecttemplate/src/main.cpp
+++ projecttemplate/src/main.cpp
@@ -7,6 +7,10 @@ void displaySensorData();
 
 void setup() 
 {
+  Serial.begin(115200);
+  Serial.println("LoRaWAN Workshop 2019");
+  Serial.println("Projekt: Wetterstation");
+
   // put your setup code here, to run once:
 }
 
@@ -16,6 +20,9 @@ void loop()
   readSensorData();
   displaySensorData();
   buildDataContainer();
+
+  Serial.println("Loop");
+  delay(2000);
 }
 
 /*
